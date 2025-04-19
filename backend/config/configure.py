f5604f5e90bf23afa1e93b27242ac01e969bf407
itsmewho <jordy_mt@live.nl>
2025-04-20 00:18:24 +0200

    sql setup

--- backend/config/configure.py
+++ backend/config/configure.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -8,3 +8,6 @@ REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT"))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None) or None
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+# -- SQL
+CONNECTION_STRING = os.getenv("DB_CONNECTION")
